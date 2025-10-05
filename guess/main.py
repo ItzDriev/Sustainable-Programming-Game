8614f5dc3910c8cac4d6126238eae7cc5737c238
Driev <158128154+ItzDriev@users.noreply.github.com>
2025-10-05 13:13:24 +0200

    Fixed shell prompts to match PIG game

--- guess/main.py
+++ guess/main.py
@@ -7,10 +7,10 @@ Lets play a game of "Pig, Dice Game".
 Your goal is to beat the opposing player 
 by reaching the target points before them!
 
-During your turn, you can roll the dice as many times as
-you'd like, however, if exactly ONE of your dice roll a '1', all your points during
-that turn are lost. If you happen to roll two '1's', all of you points will be reset
-to 0, including points acquired from previous turns.
+During your turn, you can roll the dice as many times as you'd like, 
+however, if exactly ONE of your dice roll a '1', all your points during that turn are lost. 
+If you happen to roll two '1's', all of you points will be reset to 0, 
+including points acquired from previous turns.
 
 Play carefully, and don't be a PIG!
 
