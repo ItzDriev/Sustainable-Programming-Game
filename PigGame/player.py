5cf10bf86e3da3f356bceac8d9b76a6a4ec55a73
Flurry <41346632+Flurry2005@users.noreply.github.com>
2025-10-14 17:33:16 +0200

    Updated docstrings, added so init functions shows in docs.

--- PigGame/player.py
+++ PigGame/player.py
@@ -8,19 +8,37 @@ class Player:
     """Represent a player in the game."""
 
     def __init__(self, username, user_id):
-        """Initialize the player object."""
+        """Initialize the player object.
+
+        :param username: Name of the player.
+        :type username: :py:obj:`str`
+        :param user_id: Id of player.
+        :type user_id: :py:obj:`int`
+        """
         self.score = 0
         self.__user_id = user_id
         self.__username = username
 
     def set_username(self, new_username):
-        """Set the player's username."""
+        """Set the player's username.
+
+        :param new_username: New username.
+        :type new_username: :py:obj:`str`
+        """
         self.__username = new_username
 
     def get_username(self):
-        """Return the player's username."""
+        """Return the player's username.
+
+        :return: The username of the player.
+        :rtype: :py:obj:`str`
+        """
         return self.__username
 
     def get_user_id(self):
-        """Return the player's user ID."""
+        """Return the player's user ID.
+
+        :return: The id of the player.
+        :rtype: :py:obj:`int`
+        """
         return self.__user_id
