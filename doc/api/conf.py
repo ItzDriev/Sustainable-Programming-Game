421e7fb18c845efe0afc21164cf9052a0c42d3a6
Flurry <41346632+Flurry2005@users.noreply.github.com>
2025-10-22 18:51:03 +0200

    D

--- doc/api/conf.py
+++ doc/api/conf.py
@@ -4,47 +4,47 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 # -- Project information
 print(sys.path[0])
-project = 'Sustainable Project'
-copyright = '2025, Anton Andersson'
-author = 'Anton Andersson'
+project = "Sustainable Project"
+copyright = "2025, Anton Andersson"
+author = "Anton Andersson"
 
-release = '0.1'
-version = '0.1.0'
+release = "0.1"
+version = "0.1.0"
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'myst_parser'
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
 ]
 
 autodoc_default_options = {
-    'members': True,
-    'special-members': '__init__',
+    "members": True,
+    "special-members": "__init__",
 }
 
 autosummary_generate = True
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
